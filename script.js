abef6dbaa683c207816ffe2cc5b78b2e38a74ac5
enzotoms <135044593+enzotoms@users.noreply.github.com>
2024-11-07 16:25:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ for (let i = 0; i < dropList.length; i++) {
         ? currency_code == "USD"
           ? "selected"
           : ""
-        : currency_code == "INR"
+        : currency_code == "EUR"
         ? "selected"
         : "";
     let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
